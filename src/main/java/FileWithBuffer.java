9c8396eb9ccd66e6f78e2a9122445d82d583f4b9
Oksana <briliant621621@gmail.com>
2022-07-13 18:49:14 +0300

    Changes FileWithBuffers.java

--- src/main/java/FileWithBuffer.java
+++ src/main/java/FileWithBuffer.java
@@ -1,12 +1,15 @@
 package main.java;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileWithBuffer {
-    public  void writeFile() {
+    public void writeFile() {
         File file = new File("file.txt");
-        try (FileWriter writer = new FileWriter(file))
-        {
+        try (FileWriter writer = new FileWriter(file)) {
             String text = "987-123-4567\n" +
                     "123 456 7890\n" +
                     "(123) 456-7890";
@@ -16,18 +19,24 @@ public class FileWithBuffer {
             System.out.println(e.getMessage());
         }
     }
-    public  void readFile() {
+
+    public void readFile() {
         try (FileReader reader = new FileReader("file.txt")) {
             BufferedReader readerbuf = new BufferedReader(reader);
             String line = readerbuf.readLine();
+            Pattern pattern = Pattern.compile("^((\\(\\d{3}\\))|\\d{3})[- ]?\\d{3}[-]\\d{4}$");
             while (line != null) {
-                if(line.contains("-"))
-                System.out.println(line);
-
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    System.out.println(matcher.group());
+                }
                 line = readerbuf.readLine();
             }
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 }
+
+
--- src/main/java/FileWithBuffers.java
+++ src/main/java/FileWithBuffers.java
@@ -2,8 +2,8 @@ package main.java;
 
 public class FileWithBuffers {
     public static void main(String[] args) {
-      FileWithBuffer fileWithBuffer=new FileWithBuffer();
-      fileWithBuffer.writeFile();
-     fileWithBuffer.readFile();
+        FileWithBuffer fileWithBuffer = new FileWithBuffer();
+        fileWithBuffer.writeFile();
+        fileWithBuffer.readFile();
     }
 }
